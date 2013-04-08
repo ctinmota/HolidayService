ce81d42396043027f2ee2f3e1b58641f12f7e377
crisfromnova <ctinmota@gmail.com>
2013-04-08 23:31:10 +0300

    added sources

---
+++ src/main/java/com/mycompany/hr/HumanResourceService.java
@@ -0,0 +1,12 @@
+package com.mycompany.hr;
+
+import java.util.Date;
+
+public class HumanResourceService {
+
+	public void bookHoliday(Date startDate, Date endDate, String name) {
+		System.out.println("bookHoliday(" + startDate + ", " + endDate + ", " + name);
+		
+	}
+
+}
